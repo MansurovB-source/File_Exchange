c16b4b7a2aa29320932463c2d4b1dac43812e9b5
Behruz Mansurov <mr.behruzmansurov@gmail.com>
2021-06-12 16:46:57 +0300

    file_triplet and hash (sha256)

---
+++ file_triplet.c
@@ -0,0 +1,21 @@
+//
+// Created by behruz on 12.06.2021.
+//
+
+#include "file_triplet.h"
+
+void file_triplet_destroy(struct file_triplet *triplet) {
+    free(triplet->filename);
+    free(triplet);
+}
+
+void calc_hash(FILE *file, uint8_t *hash) {
+    SHA256_CTX sha256_ctx;
+    SHA256_Init(&sha256_ctx);
+    char data[1024];
+    uint64_t bytes;
+    while ((bytes = fread(data, 1, 1024, file)) != 0) {
+        SHA256_Update(&sha256_ctx, data, bytes);
+    }
+    SHA256_Final(hash, &sha256_ctx);
+}
---
+++ file_triplet.h
@@ -0,0 +1,22 @@
+//
+// Created by behruz on 12.06.2021.
+//
+
+#ifndef FILE_EXCHANGE_FILE_TRIPLET_H
+#define FILE_EXCHANGE_FILE_TRIPLET_H
+
+#include <stdint.h>
+#include <openssl/sha.h>
+#include <malloc.h>
+
+
+struct file_triplet {
+    char *filename;
+    uint64_t filesize;
+    uint8_t hash[SHA256_DIGEST_LENGTH];
+};
+
+void file_triplet_destroy(struct file_triplet *triplet);
+void calc_hash(FILE *file, uint8_t *hash);
+
+#endif //FILE_EXCHANGE_FILE_TRIPLET_H
